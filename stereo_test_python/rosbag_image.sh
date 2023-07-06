11c74ba2c46ab83080f5fe8266c5df4ba5e19a9d
9iant <baegs94@unist.ac.kr>
2023-07-06 22:11:30 +0900

    chaos

---
+++ stereo_test_python/rosbag_image.sh
@@ -0,0 +1,9 @@
+rosbag record -O airsim_images \
+/stereo_robot/mobile_base/camera/left/image_raw/compressed \
+/stereo_robot/mobile_base/camera/right/image_raw/compressed \
+/stereo_robot/mobile_base/camera/left/camera_info \
+/stereo_robot/mobile_base/camera/right/camera_info
+
+
+
+
